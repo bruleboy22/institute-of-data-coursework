46a8c5021b696f508403e3422bb35cc51c0b6e7d
bruleboy22 <steve_e_01@yahoo.com>
2023-06-20 15:29:42 -0500

    My json Practice

---
+++ module-1/jsonPractice.js
@@ -0,0 +1,10 @@
+const bernardsBook = {
+    "book_title" : "Bernard's Book",
+    "book_description" : "This book is a biography of Steven Bernard's life",
+    "author" : "Lacie Bernard",
+    "pages" : "377"
+}
+console.log(bernardsBook.book_title);
+console.log(bernardsBook.book_description);
+console.log(bernardsBook.author);
+console.log(bernardsBook.pages);
