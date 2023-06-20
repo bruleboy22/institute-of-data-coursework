10c31240abda1eaddc3a2b1df5db2b7bb62ba0a1
bruleboy22 <steve_e_01@yahoo.com>
2023-06-19 19:07:27 -0500

    My Array Practice

---
+++ module-1/arrayPractice1.js
@@ -0,0 +1,10 @@
+let scores = new Array();
+let nbaSouthwest = ['Dallas Mavericks', 'Houston Rockets', 'Memphis Grizzlies', 'New Orleans Pelicans','San Antonio Spurs'];
+
+console.log(nbaSouthwest[0]); 'Dallas Mavericks'
+console.log(nbaSouthwest[1]); 'Houston Rockets'
+console.log(nbaSouthwest[2]); 'Memphis Grizzlies'
+console.log(nbaSouthwest[3]); 'New Orleans Pelicans'
+console.log(nbaSouthwest[4]); 'San Antonio Spurs'
+
+
