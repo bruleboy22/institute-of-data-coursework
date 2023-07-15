2591f01825d63eb5eddefa072a75726b150bbfd2
bruleboy22 <steve_e_01@yahoo.com>
2023-07-15 17:17:18 -0500

    adding excercise 2 but no work

--- module-3-lab-3/jsAdvanvanced.js
+++ module-3-lab-3/jsAdvanvanced.js
@@ -50,4 +50,22 @@ function makeCounterC(startFrom, incrementBy) {
   let counter4 = makeCounterC(0, 7);
   counter4(); 
   counter4(); 
+//-------------------------------------------------------------------------------------------------------------
+  
+
+//Excercise 2. The following delayMsg function is intended to be used to delay printing a message until
+//some time has passed.
+//a) What order will the four tests below print in? Why?
+//b) Rewrite delayMsg as an arrow function
+//c) Add a fifth test which uses a large delay time (greater than 10 seconds)
+//d) Use clearTimeout to prevent the fifth test from printing at all.
+
+function delayMsg(msg)
+{
+console.log(`This message will be printed after a delay: ${msg}`)
+}
+setTimeout(delayMsg, 100, '#1: Delayed by 100ms');
+setTimeout(delayMsg, 20, '#2: Delayed by 20ms');
+setTimeout(delayMsg, 0, '#3: Delayed by 0ms');
+delayMsg('#4: Not delayed at all')
   
