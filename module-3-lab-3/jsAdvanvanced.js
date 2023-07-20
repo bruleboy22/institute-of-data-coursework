f0e6597b5a4401a2287de912b19aa8c101be486c
bruleboy22 <steve_e_01@yahoo.com>
2023-07-20 12:59:30 -0500

    adding excercise 7

--- module-3-lab-3/jsAdvanvanced.js
+++ module-3-lab-3/jsAdvanvanced.js
@@ -225,4 +225,53 @@ multiply.delay(500)(2, 3, 4, 5); // prints 120 after 500 milliseconds
 //--------------------------------------------------------------------------------------------------------------
 
  
+// Excercie 7. In JavaScript, the toString method is used to convert an object to a string representation.
+//By default, when an object is converted to a String, it returns a string that looks something
+//like [object Object].
+//However, we can define our own toString methods for custom objects to provide a more
+//meaningful string representation.
+//a) Define a custom toString method for the Person object that will format and print their details
+//b) Test your method by creating 2 different people using the below constructor function and printing them
+//c) Create a new constructor function Student that uses call to inherit from Person and
+//add an extra property cohort
+//d) Add a custom toString for Student objects that formats and prints their details. Test with 2 students.
+
+class Person {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  toString() {
+    return `Name: ${this.name}, Age: ${this.age}, Gender: ${this.gender}`;
+  }
+}
+
+const person1 = new Person('James Brown', 73, 'male');
+const person2 = new Person('Steven Bernard', 40, 'male');
+
+console.log('person1: ' + person1);
+console.log('person2: ' + person2);
+
+class Student extends Person {
+  constructor(name, age, gender, cohort) {
+    super(name, age, gender);
+    this.cohort = cohort;
+  }
+
+  toString() {
+    return `Name: ${this.name}, Age: ${this.age}, Gender: ${this.gender}, Cohort: ${this.cohort}`;
+  }
+}
+
+const student1 = new Student('Reese', 14, 'female', '2027');
+const student2 = new Student('Torin', 10, 'male', '2032');
+
+console.log('student1: ' + student1);
+console.log('student2: ' + student2);
+
+//--------------------------------------------------------------------------------------------------------------
+
+
 
