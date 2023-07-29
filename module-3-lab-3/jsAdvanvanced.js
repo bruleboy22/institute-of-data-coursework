c14eab22c9fed49d886055e8d6622c2ac91db83a
bruleboy22 <steve_e_01@yahoo.com>
2023-07-29 07:56:30 -0500

    adding excercises 8-10

--- module-3-lab-3/jsAdvanvanced.js
+++ module-3-lab-3/jsAdvanvanced.js
@@ -274,4 +274,169 @@ console.log('student2: ' + student2);
 //--------------------------------------------------------------------------------------------------------------
 
 
+//Excercise 8. The following DigitalClock class uses an interval to print the time every second once
+//started, until stopped.
+//a) Create a new class PrecisionClock that inherits from DigitalClock and adds the
+//parameter precision – the number of ms between 'ticks'. This precision parameter
+//should default to 1 second if not supplied.
+//b) Create a new class AlarmClock that inherits from DigitalClock and adds the
+//parameter wakeupTime in the format hh:mm. When the clock reaches this time, it
+//should print a 'Wake Up' message and stop ticking. This wakeupTime parameter should
+//default to 07:00 if not supplied.
+
+class DigitalClock {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+  
+  display() {
+    let date = new Date(); // Create a new date object to get the current time
+    // Create 3 variables in one go using array destructuring
+    let [hours, mins, secs] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    if (hours < 10) hours = '0' + hours;
+    if (mins < 10) mins = '0' + mins;
+    if (secs < 10) secs = '0' + secs;
+    console.log(`${this.prefix} ${hours}:${mins}:${secs}`);
+  }
+  
+  stop() {
+    clearInterval(this.timer);
+  }
+  
+  start() {
+    this.display();
+    this.timer = setInterval(() => this.display(), 1000);
+  }
+}
+
+const myClock = new DigitalClock('my clock:');
+myClock.start();
+
+class PrecisionClock extends DigitalClock {
+  constructor(prefix, precision = 1000) {
+    super(prefix);
+    this.precision = precision;
+  }
+
+  start() {
+    this.display();
+    this.timer = setInterval(() => this.display(), this.precision);
+  }
+}
+class AlarmClock extends DigitalClock {
+  constructor(prefix, wakeupTime = "07:00") {
+      super(prefix);
+      this.wakeupTime = wakeupTime;
+  }
+  start() {
+      let date = new Date();
+      let [hours, mins] = [date.getHours(), date.getMinutes()];
+      if (hours < 10) hours = '0' + hours;
+      if (mins < 10) mins = '0' + mins;
+      let currentTime = `${hours}:${mins}`;
+      if (currentTime === this.wakeupTime) {
+          console.log("Wake Up");
+          clearInterval(this.timer);
+          return;
+      }
+      this.display();
+      this.timer = setInterval(() => {
+          let date = new Date();
+          let [hours, mins] = [date.getHours(), date.getMinutes()];
+          if (hours < 10) hours = '0' + hours;
+          if (mins < 10) mins = '0' + mins;
+          let currentTime = `${hours}:${mins}`;
+          if (currentTime === this.wakeupTime) {
+              console.log("Wake Up");
+              clearInterval(this.timer);
+              return;
+          }
+          this.display();
+      }, 1000);
+  }
+}
+
+//--------------------------------------------------------------------------------------------------------------
+
+//Excercise 9. We can delay execution of a function using setTimeout, where we need to provide both
+//the callback function and the delay after which it should execute.
+//a) Create a promise-based alternative randomDelay() that delays execution for a
+//random amount of time (between 1 and 20 seconds) and returns a promise we can use
+//via .then(), as in the starter code below
+//b) If the random delay is even, consider this a successful delay and resolve the promise,
+//and if the random number is odd, consider this a failure and reject it
+//c) Update the testing code to catch rejected promises and print a different message
+//d) Try to update the then and catch messages to include the random delay value
+
+function randomDelay() {
+  const delay = Math.floor(Math.random() * 20) + 1;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (delay % 2 === 0) {
+        resolve(`Promise resolved after ${delay} seconds`);
+      } else {
+        reject(`Promise rejected after ${delay} seconds`);
+      }
+    }, delay * 1000);
+  });
+}
+
+randomDelay()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+//--------------------------------------------------------------------------------------------------------------  
+
+
+// Excercise 10.Fetch is a browser-based function to send a request and receive a response from a server,
+//which uses promises to handle the asynchronous response.
+//The below fetchURLData uses fetch to check the response for a successful status
+//code, and returns a promise containing the JSON sent by the remote server if successful
+//or an error if it failed. (To run this code in a node.js environment, follow the instructions in
+//the comments before the function.)
+//a) Write a new version of this function using async/await
+//b) Test both functions with valid and invalid URLs
+//c) (Extension) Extend your new function to accept an array of URLs and fetch all of them,
+//using Promise.all to combine the results.
+
+//run 'npm init' and accept all the defaults
+//run 'npm install node-fetch'
+//add this line to package.json after line 5: "type": "module",
+
+const fetch = require('node-fetch');
+
+async function fetchURLData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+fetchURLData('https://jsonplaceholder.typicode.com/todos/1')
+.then(data => console.log(data))
+.catch(error => console.error(error.message));
+
+fetchURLData('https://jsonplaceholder.typicode.com/todos/invalid')
+.then(data => console.log(data))
+.catch(error => console.error(error.message));
+
+async function fetchAllURLData(urls) {
+  try {
+    const promises = urls.map(url => fetchURLData(url));
+    const results = await Promise.all(promises);
+    return results;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+fetchAllURLData(['https://jsonplaceholder.typicode.com/todos/1', 'https://jsonplaceholder.typicode.com/todos/invalid'])
+.then(data => console.log(data))
+.catch(error => console.error(error.message));
 
